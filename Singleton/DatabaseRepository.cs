7e721b9ff78a4e7c246c59a0726f43d8adf2590d
YaroslavOzornin <ipz211_oyav@student.ztu.edu.ua>
2023-03-23 22:44:19 +0200

    Singleton

---
+++ Singleton/DatabaseRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Singleton
+{
+    public sealed class DatabaseRepository
+    {
+        private static readonly DatabaseRepository instance = new DatabaseRepository();
+
+        static DatabaseRepository() { }
+
+        private DatabaseRepository() { }
+
+        public static DatabaseRepository GetInstance
+        {
+            get { return instance; }
+        }
+
+        public void DBConnect()
+        {
+            Console.WriteLine("Connected :)");
+        }
+
+        public int getSomethingFromDb()
+        {
+            return 2312122 ;
+        }
+    }
+}
---
+++ Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Singleton;
+
+Console.WriteLine("Hello, World!");
+
+DatabaseRepository db = DatabaseRepository.GetInstance;
+
+db.DBConnect();
+
+int res = db.getSomethingFromDb();
+Console.WriteLine(res);
+Console.ReadLine();
